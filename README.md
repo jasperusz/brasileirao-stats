f1908d1629f8f1def611a32db8ebb34783609f33
Jasper <235980385+jasperusz@users.noreply.github.com>
2026-05-03 21:31:28 -0300

    add pandas and dataset from kaggles

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # brasileirao-stats
 A Python project to analyse stats from Brazilian Football League using Pandas and Dataset from Kaggles
+
+Dataset: Campeonato Brasileiro de Futebol por Adão Duque (Kaggle)
---
+++ main.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+match_summary = pd.read_csv('data/campeonato-brasileiro-full.csv')
+match_stats = pd.read_csv('data/campeonato-brasileiro-estatisticas-full.csv')
+match_goals = pd.read_csv('data/campeonato-brasileiro-gols.csv')
+match_cards = pd.read_csv('data/campeonato-brasileiro-cartoes.csv')
